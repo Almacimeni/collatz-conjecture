6258c432f5c06a07999c3ae00b67fde5581cc094
Ali Can Almaçimeni <39525534+Almacimeni@users.noreply.github.com>
2019-10-28 22:41:39 +0300

    Create collatz.c

---
+++ collatz.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+
+void collatz(int sayi);
+
+int main(){
+    int sayi = 0;
+
+    do{
+        printf("Lutfen pozitif bir tamsayi giriniz: ");
+        scanf("%d",&sayi);
+    }while(sayi<=0);
+    
+    collatz(sayi);
+
+    return 0;
+}
+
+
+void collatz(int sayi){
+
+    pid_t pid = fork();
+
+    if (pid == 0)
+    {
+        printf("Child process calisiyor...\n");
+        printf("%d\n", sayi);
+        while (sayi != 1)
+        {
+            if (sayi % 2 == 0)
+            {
+                sayi = sayi / 2;
+            }
+            else if (sayi % 2 == 1)
+            {
+                sayi = 3 * (sayi) + 1;
+            }
+
+            printf("%d\n", sayi);
+        }
+
+        printf("Child process tamamlandi.\n");
+    }
+    else
+    {
+        printf("Parent process bekliyor...\n");
+        wait();
+        printf("Parent process tamamlandi.\n");
+    }
+    
+}
